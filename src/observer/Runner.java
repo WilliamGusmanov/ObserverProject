ff68defcdf8b1431356ab7582e4d080059b525d6
William Gusmanov <Williamgusmanov1@gmail.com>
2019-11-27 22:14:49 -0800

    testing commit

--- src/observer/Runner.java
+++ src/observer/Runner.java
@@ -1,11 +1,14 @@
 package observer;
 
 public class Runner {
-    Stock ORCL = new Stock();
-    Stock BA = new Stock();
-    Stock TRKX = new Stock();
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        Stock ORCL = new Stock();
+        Stock BA = new Stock();
+        Stock TRKX = new Stock();
 
-    Agent Tom = new Agent("Tom Clancey", new Stock[] {BA, TRKX});
-    Agent Robert = new Agent("Robert Mitchner", new Stock[] {ORCL, TRKX});
+        Agent Tom = new Agent("Tom Clancey", new Stock[] {BA, TRKX});
+        Agent Robert = new Agent("Robert Mitchner", new Stock[] {ORCL, TRKX});
 
+    }
 }
